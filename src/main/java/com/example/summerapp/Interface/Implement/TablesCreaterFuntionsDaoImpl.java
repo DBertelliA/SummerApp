90d984c5278e5817aa66f9fb316a73c28d1c7c29
dbertelli <useromegad@gmail.com>
2026-06-15 20:37:22 +0200

    Estructura inicial (Beta) de las funciones que quiero añadir para las tablas y su generacion

---
+++ src/main/java/com/example/summerapp/Interface/Implement/TablesCreaterFuntionsDaoImpl.java
@@ -0,0 +1,31 @@
+package com.example.summerapp.Interface.Implement;
+
+import com.example.summerapp.Interface.TablesAutoCreateAndTheirFuntionsInterface;
+
+public class TablesCreaterFuntionsDaoImpl implements TablesAutoCreateAndTheirFuntionsInterface {
+
+    @Override
+    public void addTable() {
+
+    }
+
+    @Override
+    public void dataSearch() {
+
+    }
+
+    @Override
+    public void showAllTables() {
+
+    }
+
+    @Override
+    public void deleteTables() {
+
+    }
+
+    @Override
+    public void promptExexuter() {
+
+    }
+}
---
+++ src/main/java/com/example/summerapp/Interface/TablesAutoCreateAndTheirFuntionsInterface.java
@@ -0,0 +1,9 @@
+package com.example.summerapp.Interface;
+
+public interface TablesAutoCreateAndTheirFuntionsInterface { //Too loong
+    public void addTable();
+    public void dataSearch();
+    public void showAllTables();
+    public void deleteTables();
+    public void promptExexuter();
+}
